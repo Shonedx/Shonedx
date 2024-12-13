fcdb651efb20717d970e30e59d87f59680b6c6a8
Ethan Pearce <147326430+Shonedx@users.noreply.github.com>
2024-12-14 01:10:26 +0800

    Add files via upload
     a new update about ges_cal

--- ges_cal.h
+++ ges_cal.h
@@ -12,8 +12,8 @@
 #define val 180.0/PI  //弧度制转换参数
 #define AngleRaito 436.926337 //24年11.20 这个没用到
 #define Gaito     3591/187//3508转子比,角度环控制时需要将希望的角度乘上这个
-#define t_length  0.0014//时间步长 0.0025
-#define t_length_w  0.002230 //0.0020
+#define t_length  0.0025//时间步长 0.0025
+#define t_length_w  0.0018 //0.0020
 #define StatesMaxNum 20 
 /***/
 
@@ -21,8 +21,8 @@
 //#define StepLenthMax (2*LegLenthExtremeMax*0.866f*0.9f)
 #define StepLenthMax 17
 #define LegLenthExtremeMax 29.2f //最长了,别再长了
-#define LegLenthMax 21.0f //实际上离最长腿还有一定裕度 //IMU相关，下同
-#define LegLenthMin 19.0f //有略微挤压髋关节
+#define LegLenthMax 30.0f //实际上离最长腿还有一定裕度 //IMU相关，下同
+#define LegLenthMin 11.0f //有略微挤压髋关节
 
 #define LegStandLenth 15.0f //标准站立的腿长  //2023.6.21 18.5
 #define LegCrouchLenth 10.0f //下蹲腿的长度
@@ -92,8 +92,9 @@ double t_past;
 
 typedef struct
 {
-cycloid curve; //摆动相
-cycloid support;//支撑相
+	cycloid curve; //摆动相
+	cycloid support_1;
+	cycloid support_2;//支撑相
 }Cycloid_Gait;
 /****************/
 extern	MOVE_DIR Move_Dir;
@@ -103,9 +104,9 @@ extern  Cycloid_Gait cycloid_gait;
 /*********************************************/
 
 /*******步态********/ 
-void Gait(double Tf, double x_target, double z_target, double x0, double z0, double xv0, double zf, int direction, int change, double Leglength);
+void Gait(double Tf, double x_target, double z_target, double x0, double z0, double xv0, double zf, int direction, int change, double Leglength,int gait_state);
 
-void trot(double t, double Tf, double x_target, double z_target, double x0, double z0, double xv0, double zf, double Leglength);
+void trot(double t, double Tf, double x_target, double z_target, double x0, double z0, double xv0, double zf, double Leglength );
 
 void Stand(double x_offset,double Leglength);
 
@@ -118,9 +119,15 @@ void CartesianToTheta_Cycloid_All_Legs(void); //运动学逆解所有腿
 
 void Moveleg(int direction);
 
-void swing_curve_generate(double t, double Tf, double xt, double zh, double x0, double z0, double xv0);
+//void swing_curve_generate(double t, double Tf, double xt, double zh, double x0, double z0, double xv0);
 
-void support_curve_generate(double t, double Tf, double x_past, double t_past, double zf);
+//void support_curve_generate(double t, double Tf, double x_past, double t_past, double zf);
+
+void swing_curve_generate(double t, double Tf, double xt,double x0,double z0);
+
+void support_curve_generate_1(double t, double Tf, double x_target,double x0,double z0);
+
+void support_curve_generate_2(double t, double Tf, double x_target,double x0,double z0);
 
 void Dir_Set(int direction);
 
